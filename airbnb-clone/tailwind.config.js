4cfb3da1643c33ef0c5370f8752b17b9a0499dad
FelipeFerreiraReis777 <felipeferreiradosreis777@gmail.com>
2024-10-21 20:25:12 -0300

    Initial Configa and Logo

---
+++ airbnb-clone/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{ja,ts,jsx,tsx}",
+    "./pages/**/*.{ja,ts,jsx,tsx}",
+    "./components/**/*.{ja,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
